08e4e04cb5bb33e93f7013e71d27c7403a2a0de9
Ruban Daniil <danya.ruban.2005@mail.ru>
2024-11-27 14:16:27 +0300

    no more adaptive

--- src/components/Button/ButtonForHeading.js
+++ src/components/Button/ButtonForHeading.js
@@ -2,7 +2,7 @@ export default function Button({ children, onClick, classN, isActive }) {
   return (
     <button
       onClick={onClick}
-      className={`px-10 py-1 h-16 w-56 outline-none font-bold transition-all duration-500 active:bg-black active:text-white shadow ${
+      className={`md:px-10 sm:px-3 p-0 py-1 h-16 md:w-56 sm:w-24 w-20 outline-none font-bold transition-all duration-500 active:bg-black active:text-white shadow ${
         isActive ? 'bg-black text-white rounded' : classN
       }`}
     >
--- src/components/Header.js
+++ src/components/Header.js
@@ -8,9 +8,11 @@ export default function Header({ children }) {
 
   return (
     <header className="flex bg-slate-500 justify-between items-center border-b-2 h-16 py-10 px-5 mb-10">
-      <h3 className="text-yellow-400 text-3xl font-bold inline-block">React start</h3>
+      <h3 className="text-yellow-400 text-3xl text-center font-bold inline-block">React start</h3>
       <div className="flex">{children}</div>
-      <span className="text-yellow-400 font-bold text-lg">Time: {time.toLocaleTimeString()}</span>
+      <span className="text-yellow-400 font-bold text-center text-lg">
+        Time: {time.toLocaleTimeString()}
+      </span>
     </header>
   )
 }
