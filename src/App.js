0454e73cb7cde1c7bf8996ee2e3b24666ca8b21e
Ruban Daniil <danya.ruban.2005@mail.ru>
2024-11-28 15:11:55 +0300

    Маршрутизатор

--- src/App.js
+++ src/App.js
@@ -3,42 +3,51 @@ import React, { useState } from 'react'
 import Header from './components/Header'
 import ArrayButtons from './components/ArrayButtons'
 import Ul from './components/Ul'
-import Contatcts from './components/Contatcts'
+import Contacts from './components/Contacts'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ButtonForHeading from './components/Button/ButtonForHeading'
 
 function App() {
-  const [page, setPage] = useState('main')
+  const [isActive, setIsActive] = useState('main')
 
   return (
-    <>
+    <BrowserRouter>
       <Header>
         <ButtonForHeading
-          isActive={page === 'main'}
-          onClick={() => setPage('main')}
-          classN={'bg-gray-200 rounded-l'}
+          to="/"
+          classN={isActive === 'main' ? 'rounded bg-black text-white' : ' rounded-l bg-slate-200'}
+          click={() => {
+            setIsActive('main')
+          }}
         >
           Главная
         </ButtonForHeading>
         <ButtonForHeading
-          isActive={page === 'contacts'}
-          onClick={() => setPage('contacts')}
-          classN={'bg-gray-200 rounded-r'}
+          to="/Contacts"
+          classN={
+            isActive === 'Contacts' ? 'rounded bg-black text-white' : ' rounded-r bg-slate-200'
+          }
+          click={() => {
+            setIsActive('Contacts')
+          }}
         >
           Связь
         </ButtonForHeading>
       </Header>
-
-      {page === 'main' ? (
-        <>
-          <h1 className="text-bold text-center font-bold text-3xl pb-5 border-b-2">Практика</h1>
-          <Ul />
-          <ArrayButtons />
-        </>
-      ) : (
-        <Contatcts></Contatcts>
-      )}
-      {/*Пока не изучу роутеры */}
-    </>
+      <Routes>
+        <Route
+          path="*"
+          element={
+            <>
+              <h1 className="text-bold text-center font-bold text-3xl pb-5 border-b-2">Практика</h1>
+              <Ul />
+              <ArrayButtons />
+            </>
+          }
+        />
+        <Route path="Contacts" element={<Contacts />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/components/Button/ButtonForHeading.js
+++ src/components/Button/ButtonForHeading.js
@@ -1,12 +1,13 @@
-export default function Button({ children, onClick, classN, isActive }) {
+import { Link } from 'react-router-dom'
+
+export default function Button({ children, to, classN, click }) {
   return (
-    <button
-      onClick={onClick}
-      className={`md:px-10 sm:px-3 p-0 py-1 h-16 md:w-56 sm:w-24 w-20 outline-none font-bold transition-all duration-500 active:bg-black active:text-white shadow ${
-        isActive ? 'bg-black text-white rounded' : classN
-      }`}
+    <Link
+      className={`flex items-center justify-center md:px-10 sm:px-3 p-0 py-1 h-16 md:w-56 sm:w-24 w-20 outline-none font-bold transition-all duration-500 active:bg-black active:text-white shadow ${classN}`}
+      to={to}
+      onClick={click}
     >
       {children}
-    </button>
+    </Link>
   )
 }
